4fe3e1246718ed0c81b823ee1c1b1f2bc0d5ed1a
NazmulAlamShawon <shawonkst15@gmail.com>
2023-10-29 18:17:15 +0600

    add new again file

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
+      <h1> hello world</h1>
   
-     <h1> hellow world</h1>
+      
     </main>
   )
 }
