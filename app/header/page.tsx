9ae855bc92c441260bedad562e4666f041ef3161
NazmulAlamShawon <shawonkst15@gmail.com>
2023-11-03 17:08:50 +0600

    add new

---
+++ app/header/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=''>
+        <div>logo</div>
+        <div>menu</div>
+        <div> right menu</div>
+
+    </div>
+  )
+}
+
+export default page
