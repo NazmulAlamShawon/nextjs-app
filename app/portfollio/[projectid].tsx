2051575b7182569d748d2b076d7ceee2f6ee4e57
NazmulAlamShawon <shawonkst15@gmail.com>
2023-11-05 17:16:16 +0600

    add new

---
+++ app/portfollio/[projectid].tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const projectid = () => {
+  return (
+    <div>[projectid]</div>
+  )
+}
---
+++ app/portfollio/list.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const list = () => {
+  return (
+    <div>list
+       <h2> list </h2>
+    </div>
+  )
+}
+
+export default list
