e9024207e00f5729e28dd03224f7fd7a4cbe9a13
NazmulAlamShawon <shawonkst15@gmail.com>
2023-11-07 11:29:37 +0600

    add new cate

--- app/portfollio/list.tsx
+++ app/portfollio/list.tsx
@@ -5,7 +5,7 @@ const list = () => {
     <div>
        <h2> list </h2> 
        <div>
-           <div></div>
+           <div><h2> categories</h2> </div>
            <div></div>
            <div></div>
        </div>
