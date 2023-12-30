302cd4360663a724573e1b0c2b1731fcc3043e84
Nazmul Alam <shawonkst15@gmail.com>
2023-12-30 10:15:26 +0600

    Update page.tsx

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex flex-1'>
+    <div className='flex flex-1 justify-centers'>
         <div>logo</div>
         <div>menu</div>
         <div> right menu</div>
